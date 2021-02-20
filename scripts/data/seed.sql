1545579d2145c369958cc7e9c55269f253ecc458
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-02-20 20:42:11 +0200

    Added: seed

--- scripts/data/seed.sql
+++ scripts/data/seed.sql
@@ -3,29 +3,35 @@ INSERT INTO "GroupType" ("Name", "Description", "GroupTerm", "GroupMemberTerm", 
 VALUES ('Cell', 'Cell Group', 'Group', 'Member', true);
 
 -- GROUP MEMBER ROLES --
-INSERT INTO "GroupMemberRole" ("Name", "Description", "IsLeader") VALUES ('Member', 'Group Member', false);
 INSERT INTO "GroupMemberRole" ("Name", "Description", "IsLeader") VALUES ('Cell Leader', 'Cell Leader', true);
+INSERT INTO "GroupMemberRole" ("Name", "Description", "IsLeader") VALUES ('Member', 'Group Member', false);
 
--- GROUP --
+-- GROUP -- Create 3 cells - 2 are 1 is parent of 1
 INSERT INTO "Group" ("ParentGroupId", "GroupTypeId", "ChurchId", "Name", "Description", "IsActive")
 VALUES (NULL, 1, 1, 'First Cell Group','Amazing 1st Cell', true);
 
 INSERT INTO "Group" ("ParentGroupId", "GroupTypeId", "ChurchId", "Name", "Description", "IsActive")
-VALUES (3, 1, 1, 'Split Cell Group','From 1st Cell', true);
+VALUES (1, 1, 1, 'Split Cell Group','From 1st Cell', true);
 
 INSERT INTO "Group" ("ParentGroupId", "GroupTypeId", "ChurchId", "Name", "Description", "IsActive")
 VALUES (NULL, 1, 1, 'Second Cell Group','Amazing 2nd Cell', true);
 
--- GROUP MEMBERS --
+-- GROUP MEMBERS -- 3 members of 1 cell (2 member, 1 leader)
 INSERT INTO "GroupMember" ("GroupId", "PersonId", "GroupMemberRoleId", "GroupMemberStatus", "DateTimeAdded", "IsArchived", "CommunicationPreference")
-VALUES (3, 1, 1, 'Active', CURRENT_DATE, false, 'Email' );
-
+VALUES (1, 1, 1, 'Active', CURRENT_DATE, false, 'Email' ); -- LEADER
 INSERT INTO "GroupMember" ("GroupId", "PersonId", "GroupMemberRoleId", "GroupMemberStatus", "DateTimeAdded", "IsArchived", "CommunicationPreference")
-VALUES (3, 2, 1, 'Active', CURRENT_DATE, false, 'SMS' );
-
+VALUES (1, 2, 2, 'Active', CURRENT_DATE, false, 'SMS' ); -- MEMBER 1
 INSERT INTO "GroupMember" ("GroupId", "PersonId", "GroupMemberRoleId", "GroupMemberStatus", "DateTimeAdded", "IsArchived", "CommunicationPreference")
-VALUES (3, 3, 1, 'Active', CURRENT_DATE, false, 'WhatsApp' );
+VALUES (1, 3, 2, 'Active', CURRENT_DATE, false, 'WhatsApp' ); -- MEMBER 2
 
 -- GROUP ATTENDANCE --
-INSERT INTO "GroupMemberAttendance" ("GroupMemberId", "GroupId", "AttendanceDate", "DidAttend", "IsFirstTime", "Note", "GroupAttendanceId")
-VALUES (1, 3, CURRENT_DATE, true, true, 'Filled with the Holy Spirit')
+INSERT INTO "GroupAttendance" ("GroupId", "AttendanceDate", "DidNotOccur", "AttendanceCount", "FirstTimerCount", "NewConvertCount", "Notes")
+VALUES (1, CURRENT_DATE, NULL, 3, 1, 1, 'Great first cell meeting' );
+
+-- GROUP MEMBER ATTENDANCE --
+INSERT INTO "GroupMemberAttendance" ("GroupMemberId", "GroupId", "AttendanceDate", "DidAttend", "IsFirstTime", "IsNewConvert","Note", "GroupAttendanceId")
+VALUES (1, 1, CURRENT_DATE, true, false, false, NULL, 1); -- LEADER
+INSERT INTO "GroupMemberAttendance" ("GroupMemberId", "GroupId", "AttendanceDate", "DidAttend", "IsFirstTime", "IsNewConvert","Note", "GroupAttendanceId")
+VALUES (2, 1, CURRENT_DATE, true, true, true, 'First timer gave his life to Christ', 1); -- MEMBER 1
+INSERT INTO "GroupMemberAttendance" ("GroupMemberId", "GroupId", "AttendanceDate", "DidAttend", "IsFirstTime", "IsNewConvert","Note", "GroupAttendanceId")
+VALUES (3, 1, CURRENT_DATE, true, false, false, 'Great member', 1); -- MEMBER 2
