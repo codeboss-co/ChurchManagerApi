7397c12b19f82ff172c5cf251c62b81dc088dd08
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-03-29 21:46:49 +0200

    Updated: sql queries

--- scripts/data/church_attendance_queries.sql
+++ scripts/data/church_attendance_queries.sql
@@ -1,3 +1,7 @@
+-- https://kb.objectrocket.com/postgresql/how-to-use-postgresql-lag-function-674
+-- https://www.postgresqltutorial.com/postgresql-lag-function/
+
+-- EF CORE QUERY
 SELECT
        date_part('year', c."AttendanceDate")::INT AS "Year",
        date_part('month', c."AttendanceDate")::INT AS "Month",
@@ -11,58 +15,153 @@ WHERE (c."AttendanceDate" >= '2020-09-1') AND (c."AttendanceDate" <= '2020-12-31
 GROUP BY date_part('year', c."AttendanceDate")::INT, date_part('month', c."AttendanceDate")::INT
 ORDER BY date_part('year', c."AttendanceDate")::INT DESC, date_part('month', c."AttendanceDate")::INT DESC
 
-
-SELECT
-       date_part('year', c."AttendanceDate")::INT AS "Year",
-       date_part('month', c."AttendanceDate")::INT AS "Month",
-       COALESCE(SUM(c."AttendanceCount"), 0)::INT AS "TotalAttendance",
-       c."ChurchId" AS "ChurchId",
-	LAG(COALESCE(SUM(c."AttendanceCount"), 0),1) OVER (
-		PARTITION BY "ChurchId"
-		ORDER BY date_part('year', c."AttendanceDate")::INT DESC
-	) previous_year_sales
-FROM "Churches"."ChurchAttendance" AS c
-    GROUP BY date_part('year', c."AttendanceDate")::INT
-;
-
-
--- https://www.postgresqltutorial.com/postgresql-lag-function/
+-- LAG BASIC
 WITH cte AS (
-	SELECT
-        date_part('year', c."AttendanceDate") AS "Year",
-		COALESCE(SUM(c."AttendanceCount"), 0) AS "TotalAttendance"
-    FROM "Churches"."ChurchAttendance" AS c
-	GROUP BY "Year"
-	ORDER BY "Year"
+   SELECT
+        date_part('year', "AttendanceDate") AS Year,
+        date_part('month', "AttendanceDate") AS Month,
+		COALESCE(SUM("AttendanceCount"), 0) AS TotalAttendance
+   FROM "Churches"."ChurchAttendance"
+	GROUP BY Year, Month
+	ORDER BY Year, Month
 )
 SELECT
-	"Year",
-	"TotalAttendance",
-	LAG("TotalAttendance",1) OVER ( ORDER BY "Year"	) previous_year_sales
+   Year,
+   Month,
+   TotalAttendance,
+   LAG(TotalAttendance, 1) OVER (
+      ORDER BY Year
+   ) previous_year_attendance
 FROM
-	cte;
+   cte;
 
--- VARIANCE --
+
+-- WITH VARIANCE
 
 WITH cte AS (
-	SELECT
-        date_part('year', c."AttendanceDate") AS "Year",
-		COALESCE(SUM(c."AttendanceCount"), 0) AS "TotalAttendance"
-    FROM "Churches"."ChurchAttendance" AS c
-	GROUP BY "Year"
-	ORDER BY "Year"
+   SELECT
+        date_part('year', "AttendanceDate") AS Year,
+        date_part('month', "AttendanceDate") AS Month,
+		COALESCE(SUM("AttendanceCount"), 0) AS TotalAttendance
+   FROM "Churches"."ChurchAttendance"
+	GROUP BY Year, Month
+	ORDER BY Year, Month
 ), cte2 AS (
-	SELECT
-        "Year",
-        "TotalAttendance",
-        LAG("TotalAttendance",1) OVER ( ORDER BY "Year"	) previous_year_sales
-	FROM
-		cte
+   SELECT
+       Year,
+       Month,
+       TotalAttendance,
+       LAG(TotalAttendance, 1) OVER (
+          ORDER BY Year DESC , Month DESC
+       ) previous_year_attendance
+   FROM
+      cte
 )
 SELECT
-    "Year",
-    "TotalAttendance",
-	previous_year_sales,
-	(previous_year_sales - "TotalAttendance") variance
+   Year,
+   Month,
+   TotalAttendance,
+   previous_year_attendance,
+   (TotalAttendance - previous_year_attendance) AS variance
 FROM
-	cte2;
+   cte2;
+
+
+-- WITH PARTITION
+
+WITH cte AS (
+   SELECT
+        date_part('year', "AttendanceDate") AS Year,
+        date_part('month', "AttendanceDate") AS Month,
+        "ChurchId" AS ChurchId,
+		COALESCE(SUM("AttendanceCount"), 0) AS TotalAttendance
+   FROM "Churches"."ChurchAttendance"
+	GROUP BY Year, Month, "ChurchId"
+	ORDER BY Year, Month
+), cte2 AS (
+   SELECT
+       Year,
+       Month,
+       ChurchId,
+       TotalAttendance,
+       LAG(TotalAttendance, 1) OVER (
+          PARTITION BY ChurchId
+          ORDER BY Year , Month
+       ) previous_year_attendance
+   FROM
+      cte
+)
+SELECT
+   Year,
+   Month,
+   ChurchId,
+   TotalAttendance,
+   previous_year_attendance,
+   (TotalAttendance - previous_year_attendance) AS variance
+FROM
+   cte2;
+
+
+-- ATTENDANCE BREAKDOWN WITH PERCENTAGE
+
+WITH cte AS (
+   SELECT
+        date_part('year', "AttendanceDate") AS Year,
+        date_part('month', "AttendanceDate") AS Month,
+        "ChurchId" AS ChurchId,
+		COALESCE(SUM("MalesCount"), 0) AS TotalMales,
+		COALESCE(SUM("FemalesCount"), 0) AS TotalFemales,
+		COALESCE(SUM("ChildrenCount"), 0) AS TotalChildren
+   FROM "Churches"."ChurchAttendance"
+	GROUP BY Year, Month, "ChurchId"
+	ORDER BY Year , Month
+), cte2 AS (
+   SELECT
+       Year,
+       Month,
+       ChurchId,
+       TotalMales,
+       TotalFemales,
+       TotalChildren,
+       -- MALES
+       LAG(TotalMales, 1) OVER (
+          PARTITION BY ChurchId
+          ORDER BY Year , Month
+       ) previous_month_males_attendance,
+       -- FEMALES
+       LAG(TotalFemales, 1) OVER (
+          PARTITION BY ChurchId
+          ORDER BY Year , Month
+       ) previous_month_females_attendance,
+       -- CHILDREN
+       LAG(TotalChildren, 1) OVER (
+          PARTITION BY ChurchId
+          ORDER BY Year , Month
+       ) previous_month_children_attendance
+   FROM
+      cte
+)
+SELECT
+   Year,
+   Month,
+   ChurchId,
+   -- MALES
+   TotalMales,
+   previous_month_males_attendance,
+   (TotalMales - COALESCE(previous_month_males_attendance, 0)) AS variance_males,
+   ((TotalMales - COALESCE(previous_month_males_attendance, 0)::FLOAT) / TotalMales::FLOAT * 100)::FLOAT AS variance_males_percentage,
+   -- FEMALES
+   TotalFemales,
+   previous_month_females_attendance,
+   (TotalFemales - COALESCE(previous_month_females_attendance, 0)) AS variance_males,
+   ((TotalFemales - COALESCE(previous_month_females_attendance, 0)::FLOAT) / TotalFemales::FLOAT * 100)::FLOAT AS variance_females_percentage,
+  -- CHILDREN
+   TotalChildren,
+   previous_month_females_attendance,
+   (TotalChildren - COALESCE(previous_month_children_attendance, 0)) AS variance_males,
+   ((TotalChildren - COALESCE(previous_month_children_attendance, 0)::FLOAT) / TotalChildren::FLOAT * 100)::FLOAT AS variance_children_percentage
+FROM
+   cte2;
+
+
+
