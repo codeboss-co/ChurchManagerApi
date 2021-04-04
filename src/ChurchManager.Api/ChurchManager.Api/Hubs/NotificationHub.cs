e3b1485d0c58a939d5d3a2ad31407cf240279e03
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-04-04 02:41:43 +0200

    Added: working OnlineUser

--- src/ChurchManager.Api/ChurchManager.Api/Hubs/NotificationHub.cs
+++ src/ChurchManager.Api/ChurchManager.Api/Hubs/NotificationHub.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using ChurchManager.Domain.Features.People.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
@@ -11,6 +16,9 @@ namespace ChurchManager.Api.Hubs
     {
         private readonly IPersonDbRepository _dbRepository;
         private readonly ILogger<NotificationHub> _logger;
+
+        private static readonly ConcurrentBag<NotificationConnection> NotificationConnections = new();
+
 
         public NotificationHub(
             IPersonDbRepository dbRepository,
@@ -39,6 +47,56 @@ namespace ChurchManager.Api.Hubs
 
             await Clients.User(Context.UserIdentifier).SendAsync("DirectMessage", notification);
             await base.OnConnectedAsync();
+
+            // Extra
+            var connectionId = Context.ConnectionId;
+            var userId = Context.UserIdentifier;
+            if (NotificationConnections.All(p => p.UserId != userId))
+            {
+                NotificationConnections.Add(new NotificationConnection { UserId = userId, ConnectionId = connectionId });
+            }
+
+            var onlineUser = new
+            {
+                Id = userId,
+                Name = $"{person.FullName.FirstName} {person.FullName.LastName}",
+                Avatar = person.PhotoUrl,
+                Status = "online",
+                Unread = 2
+            };
+
+            await Clients.All.SendAsync("OnlineUser", onlineUser);
         }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            var userId = Context.UserIdentifier;
+            var item = NotificationConnections.FirstOrDefault(p => p.UserId == userId);
+            if(item != null && NotificationConnections.TryTake(out item))
+            {
+                _logger.LogDebug("[x] NotificationHub Disconnected for {user} with Connection: {connection}", Context.UserIdentifier, Context.ConnectionId);
+                
+                var person = await _dbRepository.ProfileByUserLoginId(Context.UserIdentifier);
+                var onlineUser = new
+                {
+                    Id = userId,
+                    Name = $"{person.FullName.FirstName} {person.FullName.LastName}",
+                    Avatar = person.PhotoUrl,
+                    Status = "offline",
+                    Unread = 2
+                };
+
+                await Clients.All.SendAsync("OnlineUser", onlineUser);
+            }
+
+            await base.OnDisconnectedAsync(ex);
+        }
+
+    }
+
+    public class NotificationConnection
+    {
+        public string ConnectionId { get; set; }
+        public string UserId { get; set; }
     }
 }
