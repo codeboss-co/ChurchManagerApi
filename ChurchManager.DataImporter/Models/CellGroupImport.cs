1fb739277b708170b14ce2e5960194e0e4b6c0de
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-04-29 04:40:43 +0200

    Fix: group importer

--- ChurchManager.DataImporter/Models/CellGroupImport.cs
+++ ChurchManager.DataImporter/Models/CellGroupImport.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChurchManager.Persistence.Models.Churches;
+using ChurchManager.Persistence.Models.Groups;
 
 namespace ChurchManager.DataImporter.Models
 {
@@ -8,8 +12,20 @@ namespace ChurchManager.DataImporter.Models
         public string Description { get; set; }
         public string Address { get; set; }
         public bool? IsOnline { get; set; }
-        public string ParentGroup { get; set; }
+        public string ParentGroupName { get; set; }
         public string Church { get; set; }
         public DateTimeOffset? StartDate { get; set; }
+
+        public static Group ToEntity(CellGroupImport import, IList<Church> churches, IList<Group> groups = null) => new()
+        {
+            Name = import.Name,
+            Description = import.Description,
+            Address = import.Address,
+            ChurchId = churches.FirstOrDefault(x => x.Name == import.Church)?.Id,
+            IsOnline = import.IsOnline,
+            StartDate = import.StartDate,
+            GroupTypeId = 1,
+            ParentGroupId = groups?.FirstOrDefault(x => x.Name == import.ParentGroupName)?.Id
+        };
     }
 }
