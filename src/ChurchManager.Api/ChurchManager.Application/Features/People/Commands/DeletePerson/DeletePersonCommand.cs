8db6664fc7ccf4e3f1ad788f11b6bb65fa0831f9
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-07-17 01:03:22 +0200

    Added: delete person

--- src/ChurchManager.Api/ChurchManager.Application/Features/People/Commands/DeletePerson/DeletePersonCommand.cs
+++ src/ChurchManager.Api/ChurchManager.Application/Features/People/Commands/DeletePerson/DeletePersonCommand.cs
@@ -1,60 +1,86 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using ChurchManager.Application.Wrappers;
 using ChurchManager.Domain.Features.People;
 using ChurchManager.Domain.Features.People.Repositories;
 using ChurchManager.Infrastructure.Abstractions.Persistence;
+using CodeBoss.Extensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ChurchManager.Application.Features.People.Commands.DeletePerson
 {
     /// <summary>
     /// This will hard delete person and also remove from all groups 
     /// </summary>
-    public record DeletePersonCommand(int PersonId) : IRequest;
+    public record DeletePersonCommand(int PersonId) : IRequest<ApiResponse>;
 
-    public class DeletePersonHandler : IRequestHandler<DeletePersonCommand>
+    public class DeletePersonHandler : IRequestHandler<DeletePersonCommand, ApiResponse>
     {
         private readonly IPersonDbRepository _dbRepository;
         private readonly IGenericDbRepository<Family> _familyRepository;
+        private readonly ILogger<DeletePersonHandler> _logger;
 
         public DeletePersonHandler(
             IPersonDbRepository dbRepository,
-            IGenericDbRepository<Family> familyRepository)
+            IGenericDbRepository<Family> familyRepository,
+            ILogger<DeletePersonHandler> logger)
         {
             _dbRepository = dbRepository;
             _familyRepository = familyRepository;
+            _logger = logger;
         }
 
-        public async Task<Unit> Handle(DeletePersonCommand command, CancellationToken ct)
+        public async Task<ApiResponse> Handle(DeletePersonCommand command, CancellationToken ct)
         {
-            var person = await _dbRepository.GetByIdAsync(command.PersonId, ct);
-
-            if (person is not null)
+            try
             {
-                await _dbRepository.DeleteAsync(person, ct);
+                var person = await _dbRepository
+                       .Queryable()
+                       .Include(x => x.PhoneNumbers)
+                       .Include(x => x.Notes)
+                       .FirstOrDefaultAsync(x => x.Id == command.PersonId, ct);
 
-                // Delete family if this is the last family member
-                if (person.FamilyId.HasValue)
+                if(person is not null)
                 {
-                    // Check for family members
-                    var isLastFamilyMember = !await _dbRepository
-                        .Queryable()
-                        .AsNoTracking()
-                        .AnyAsync(x => x.FamilyId == person.FamilyId, ct);
+                    await _dbRepository.DeleteAsync(person, ct);
 
-                    if (isLastFamilyMember)
+                    // Delete family if this is the last family member
+                    if(person.FamilyId.HasValue)
                     {
-                        var family = await _familyRepository.GetByIdAsync(person.FamilyId, ct);
-                        if (family is not null)
+                        // Check for family members
+                        var isLastFamilyMember = !await _dbRepository
+                            .Queryable()
+                            .AsNoTracking()
+                            .AnyAsync(x => x.FamilyId == person.FamilyId, ct);
+
+                        if(isLastFamilyMember)
                         {
-                            await _familyRepository.DeleteAsync(family, ct);
+                            var family = await _familyRepository.GetByIdAsync(person.FamilyId, ct);
+                            if(family is not null)
+                            {
+                                await _familyRepository.DeleteAsync(family, ct);
+                            }
                         }
+                    }
+
+                    // Cleanup
+                    if (!person.UserLoginId.IsNullOrEmpty())
+                    {
+                        // TODO: delete user logins 
                     }
                 }
             }
+            catch(Exception ex)
+            {
+                var message = $"Problem deleting person with Id: {command.PersonId}";
+                _logger.LogError(ex, message);
+                return new ApiResponse(message);
+            }
 
-            return Unit.Value;
+            return new ApiResponse(true);
         }
     }
 }
