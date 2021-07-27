220e8c6a48d9f596f31684541e4ab848fc4780c0
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-07-27 10:54:46 +0200

    Added: SetIsOriginAllowedToAllowWildcardSubdomains

--- src/ChurchManager.Api/ChurchManager.Api/Extensions/ServiceExtensions.cs
+++ src/ChurchManager.Api/ChurchManager.Api/Extensions/ServiceExtensions.cs
@@ -84,17 +84,17 @@ namespace ChurchManager.Api.Extensions
             // TODO: Add specific environments
             services.AddCors(options =>
             {
+                //  If you need the top level as well as subdomains, you need to add both -> e.g. .WithOrigins(new string[] { "https://*.example.com", "https://example.com" }) 
                 options.AddPolicy(ApiRoutes.DefaultCorsPolicy,
                     builder => builder
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
                         .WithOrigins(
                             "http://localhost:4200", // Angular App
                             "http://localhost:8080", // Angular PWA test
-                            "http://test-churchmanager.codeboss.tech", // Test
-                            "https://test-churchmanager.codeboss.tech", // Test
-                            "http://test-churchmanager-api.codeboss.tech", // Test
-                            "https://test-churchmanager-api.codeboss.tech", // Test
-                            "http://churchmanager.codeboss.tech", // Production
-                            "https://churchmanager.codeboss.tech" // Production
+                            "http://*.codeboss.tech", // HTTP
+                            "https://*.codeboss.tech", // HTTPS
+                            "http://codeboss.tech", // TOP LEVEL DOMAIN
+                            "https://codeboss.tech" // TOP LEVEL DOMAIN
                             ) 
                         .AllowAnyMethod()
                         .AllowAnyHeader()
