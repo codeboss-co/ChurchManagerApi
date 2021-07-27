9ddcf7ae0b4f7f65ef73487ae76c55f18ca84186
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-07-27 12:18:16 +0200

    Added: dxoazadshajgs.cloudfront.net

--- src/ChurchManager.Api/ChurchManager.Api/Extensions/ServiceExtensions.cs
+++ src/ChurchManager.Api/ChurchManager.Api/Extensions/ServiceExtensions.cs
@@ -95,7 +95,9 @@ namespace ChurchManager.Api.Extensions
                             "https://*.codeboss.tech", // HTTPS
                             "http://codeboss.tech", // TOP LEVEL DOMAIN
                             "https://codeboss.tech", // TOP LEVEL DOMAIN
-                            "http://*.codeboss.tech.s3-website-us-east-1.amazonaws.com" // S3  buckets
+                            "http://*.codeboss.tech.s3-website-us-east-1.amazonaws.com", // S3 buckets
+                            "http://dxoazadshajgs.cloudfront.net", // Test Cloud front
+                            "https://dxoazadshajgs.cloudfront.net" // Test Cloud front
                             ) 
                         .AllowAnyMethod()
                         .AllowAnyHeader()
