7710739bc998fc781dbac5507e7c068db0851b81
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-08-18 00:20:25 +0200

    Added: email test

--- test/ChurchManager.Infrastructure.Tests/EmailSender_Tests.cs
+++ test/ChurchManager.Infrastructure.Tests/EmailSender_Tests.cs
@@ -10,12 +10,19 @@ namespace ChurchManager.Infrastructure.Tests
 {
     public class EmailSender_Tests
     {
+        private string _awsAccessKeyId = "AKIA6HLAUN2QAA2ZQ5W3";
+        private string _awsSecretAccessKey = "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";
+        private string _toEmailAddress = "dillancagnetta@yahoo.com";
+
+        private string _templateFilePath = "./Templates/ExampleTemplate.liquid";
+        private string _templateExampleData = "./Templates/ExampleData.json";
+
         [Fact]
         public async Task Test_sending_emails()
         {
-            var sender = new AwsSesEmailSender("AKIA6HLAUN2QAA2ZQ5W3", "PMd1zeIjRc2Nja0qwa+RzU3y5/xdqolEDODGQoqX");
+            var sender = new AwsSesEmailSender(_awsAccessKeyId, _awsSecretAccessKey);
 
-            var operationResult = await sender.SendEmailAsync("dillancagnetta@yahoo.com", "EmailSender Tests", "Hello there");
+            var operationResult = await sender.SendEmailAsync(_toEmailAddress, "EmailSender Basic Test", "Hello there");
 
             Assert.True(operationResult.IsSuccess);
         }
@@ -23,16 +30,33 @@ namespace ChurchManager.Infrastructure.Tests
         [Fact]
         public void Test_template_parser()
         {
-            string templateFilePath = "./Templates/ExampleTemplate.liquid";
-            string template = File.ReadAllText(templateFilePath);
+            string template = File.ReadAllText(_templateFilePath);
 
-            ExampleData  data = ExampleData("./Templates/ExampleData.json");
+            ExampleData  data = ExampleData(_templateExampleData);
             object model = new { Model = data };
 
             var parser = new DotLiquidTemplateParser();
             var result = parser.Render(template, model);
 
             Assert.Contains("Mr Dillan Cagnetta", result);
+        }
+
+        [Fact]
+        public async Task Test_send_templated_email()
+        {
+            string template = await File.ReadAllTextAsync(_templateFilePath);
+
+            ExampleData data = ExampleData(_templateExampleData);
+            object model = new { Model = data };
+
+            var parser = new DotLiquidTemplateParser();
+            var htmlBody = parser.Render(template, model);
+
+            var sender = new AwsSesEmailSender(_awsAccessKeyId, _awsSecretAccessKey);
+
+            var operationResult = await sender.SendEmailAsync(_toEmailAddress, "EmailSender Template Test", htmlBody);
+
+            Assert.True(operationResult.IsSuccess);
         }
 
         private ExampleData ExampleData(string dataFilePath)
