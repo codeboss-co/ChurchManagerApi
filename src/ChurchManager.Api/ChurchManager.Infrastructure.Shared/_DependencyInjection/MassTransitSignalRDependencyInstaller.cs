b28a9b95b1c2229e2328719018047eca1ee2423e
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-10-04 14:32:01 +0200

    Added MassTransit SignalrR services

--- src/ChurchManager.Api/ChurchManager.Infrastructure.Shared/_DependencyInjection/MassTransitSignalRDependencyInstaller.cs
+++ src/ChurchManager.Api/ChurchManager.Infrastructure.Shared/_DependencyInjection/MassTransitSignalRDependencyInstaller.cs
@@ -1,9 +1,6 @@
 ﻿using ChurchManager.Domain.Features.Communication.Services;
 using ChurchManager.Infrastructure.Shared.SignalR;
-using Codeboss.Types;
 using CodeBoss.AspNetCore.DependencyInjection;
-using CodeBoss.MassTransit.Services;
-using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
