d9d845a35efc269dd381a15c49699068fe8005ec
Dillan Cagnetta <dillan.cagnetta@structureit.net>
2022-07-25 18:33:47 +0200

    Updated to use AWS EnvironmentVariablesAWSCredentials

--- src/API/ChurchManager.Api/Program.cs
+++ src/API/ChurchManager.Api/Program.cs
@@ -14,9 +14,14 @@ namespace ChurchManager.Api
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureServices(services => services.AddConvey().Build())
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddEnvironmentVariables();
+                } )
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
@@ -24,31 +29,41 @@ namespace ChurchManager.Api
                 .ConfigureAppConfiguration((context, config) =>
                 {
                     var environmentName = context.HostingEnvironment.EnvironmentName;
-                    Console.WriteLine($"** Environment: [{environmentName}], ASPNETCORE_ENVIRONMENT: [{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}] **");
-                    // We pull settings from AWS parameter store if not running locally
-                    if(!context.HostingEnvironment.IsDevelopment())
-                    {
-                        config
-                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                            .AddJsonFile($"appsettings.{environmentName}.json", true, true);
-
-                        var configuration = config.Build();
-
-                        // AWS Configuration
-                        var awsOptions = configuration.GetAWSOptions();
-                        var accessKey = configuration["AWS:AccessKey"];
-                        var secretKey = configuration["AWS:SecretKey"];
-                        awsOptions.Credentials = new BasicAWSCredentials(accessKey, secretKey);
-                        // AWS Parameter Store
-                        config.AddSystemsManager(
-                            path: $"/{AppName}/{environmentName}", 
-                            awsOptions: awsOptions,
-                            reloadAfter: TimeSpan.FromMinutes(5));
-                    }
+                    Console.WriteLine($"** Environment: [{environmentName}], " +
+                                      $"ASPNETCORE_ENVIRONMENT: [{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}] **" +
+                                      $"AWS_ACCESS_KEY : [{Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID")}] **"
+                    );
+                    // Pull settings from AWS parameter store
+                    ConfigureAwsParameterStore(config, environmentName);
 
                     // https://github.com/npgsql/efcore.pg/issues/2000
                     AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
                 })
                 .UseLogging();
+
+            void ConfigureAwsParameterStore(IConfigurationBuilder configurationBuilder, string environment)
+            {
+                configurationBuilder
+                    .AddEnvironmentVariables()
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{environment}.json", true, true);
+
+                var configuration = configurationBuilder.Build();
+
+                // AWS Configuration
+                var awsOptions = configuration.GetAWSOptions();
+                //var accessKey = configuration["AWS:AccessKey"];
+                //var secretKey = configuration["AWS:SecretKey"];
+                //awsOptions.Credentials = new BasicAWSCredentials(accessKey, secretKey);
+                awsOptions.Credentials = new EnvironmentVariablesAWSCredentials();
+
+                // AWS Parameter Store
+                configurationBuilder.AddSystemsManager(
+                    path: $"/{AppName}/{environment}",
+                    awsOptions: awsOptions,
+                    reloadAfter: TimeSpan.FromMinutes(5));
+            }
+
+        }
     }
 }
